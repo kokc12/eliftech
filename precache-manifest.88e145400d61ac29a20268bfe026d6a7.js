df746a98ff25815d3cd0d4c3c8d9377654331d00
kokc12 <kokc12yura@gmail.com>
2019-08-08 16:47:36 +0300

    Updates

---
+++ precache-manifest.88e145400d61ac29a20268bfe026d6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b88558d527b0c9e9c54f0c0a55d88bf",
+    "url": "/eliftech/index.html"
+  },
+  {
+    "revision": "8b1be7d8f90f6b5a2489",
+    "url": "/eliftech/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "eaaf6c5e51edcb64a665",
+    "url": "/eliftech/static/css/main.6942973f.chunk.css"
+  },
+  {
+    "revision": "8b1be7d8f90f6b5a2489",
+    "url": "/eliftech/static/js/2.f9c59c02.chunk.js"
+  },
+  {
+    "revision": "eaaf6c5e51edcb64a665",
+    "url": "/eliftech/static/js/main.2bb09857.chunk.js"
+  },
+  {
+    "revision": "c8dd443bc515ce7e1544",
+    "url": "/eliftech/static/js/runtime~main.f77e8982.js"
+  }
+]);
